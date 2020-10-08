47a3f184480b560e7bfe44d8436c21a70f809c30
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-08 12:48:50 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -17,3 +17,10 @@ example:
 Enter number:4
 4 8 12 16 20 24 28 32 36 40 44 48
 """
+
+num=int(input("Enter a number: "))
+numList=(str(num*1),str(num*2),str(num*3),str(num*4),str(num*5),str(num*6),str(num*7),str(num*8),str(num*9),str(num*10),str(num*11),str(num*12))
+output = ""
+for i in numList:
+    output=output+str(i)+ " "
+print(output)    
