a23b82cac554d08c60eaf484edb3a84863a50ad5
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-08 12:47:13 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -26,3 +26,11 @@ example:
 Enter a number: 4
 4! is 24
 """
+num=int(input("Enter a number: "))
+num1=0
+num2=1
+for i in range (0,num):
+    num1=num-i
+    num2=num1*num2
+
+print(str(num)+"! is "+str(num2) +" where "+str(num)+" is the integer entered and " +str(num2)+ " is the calculated answer")
