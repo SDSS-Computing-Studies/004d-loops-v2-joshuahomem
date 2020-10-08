d182fd837a437596b3bd67847a2191d0af1a986a
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-08 12:48:18 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -17,3 +17,20 @@ example:
 enter a number: 4
 the sum of the series is 1234
 """
+num=int(input("Enter an integer smaller than 10: "))
+if num>=10:
+    print("That integer is larger than 10")
+    quit()
+digit="1"
+a=0
+b=0
+c=0
+d=0
+for i in range(1,num+1):
+    
+    b=int(i*digit)
+    d=b+c
+    c=d
+    
+    
+print("the sum of the series is "+str(d))
